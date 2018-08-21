834109e4e80eb07a14b85c8c55fbe80f34f6f7c8
Ed Brennan <ejbrennan@gmail.com>
2018-08-20 20:02:32 -0400

    Mistake for Travis

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
-    });
+    })
   })
 
   // Create a new example
