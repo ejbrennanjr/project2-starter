67790c84781ef9a4297c94566c00e0e5af1d023b
Ed Brennan <ejbrennan@gmail.com>
2018-08-25 00:08:16 -0400

    Basic version of a passport secured site supporting Facebook strategy

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
+var ensureLogin = require("connect-ensure-login");
 var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", ensureLogin.ensureLoggedIn("/login"), function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
+        user: req.user,
         msg: "Welcome!",
         examples: dbExamples
       });
@@ -12,13 +14,22 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/example/:id", ensureLogin.ensureLoggedIn("/login"), function(
+    req,
+    res
+  ) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.render("example", {
         example: dbExample
       });
+    });
+  });
+
+  app.get("/profile", ensureLogin.ensureLoggedIn("/login"), function(req, res) {
+    res.render("profile", {
+      user: req.user
     });
   });
 
